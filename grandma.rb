7b4e4b96841d06dc776c6976966a1fba37d313fd
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-09 21:15:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,8 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN"
   elsif phrase  == "Hi Nana, how are you?" || phrase == "Hi!"
     return 'HUH?! SPEAK UP, SONNY!'
-  elsif phrase == "Hi  
+  elsif phrase == 'WHAT DID YOU EAT TODAY?'
+    return 
 end
 
 # Whatever you say to grandma, she should respond with
